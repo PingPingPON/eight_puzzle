1e7529c0079373b1271f3f5647e7fd5ee472bad6
PingPingPON <dyfcocoa@outlook.com>
2020-05-05 17:48:51 +0800

    initial

--- main.cpp
+++ main.cpp
@@ -4,50 +4,50 @@
 #pragma once
 #include "puzzle.h"
 
-int main() {
-    fstream ifs;
-    fstream ofs;
-    string str = "start.txt";
-    if (!openFile(ifs, str, ios::in)) {
-        cout << "file is not opened" << endl;
-        return ERR;
-    }
-    str = "result.txt";
-    if (!openFile(ofs, str, ios::out)) {
-        cout << "file is not opened" << endl;
-    }
-
-    int *pointer;
-    table s0{}, target{};
-    pointer = &(s0.table[0][0]);
-    for (int i = 0; i < 9; i++) {
-        ifs >> *pointer;
-        if (*pointer++ == 0) {
-            s0.row = i / 3;
-            s0.col = i % 3;
-        }
-    }
-    pointer = &(target.table[0][0]);
-    for (int i = 0; i < 9; i++) {
-        ifs >> *pointer;
-        if (*pointer++ == 0) {
-            target.row = i / 3;
-            target.col = i % 3;
-        }
-    }
-
-    vector<table> vt_out;
-    int count=puzzle(s0, target, vt_out, false);
-
-    if(count==ERR){
-        ofs << "目标状态不可达" << endl;
-        return ERR;
-    }
-
-    for(int i=0;i<vt_out.size();i++)
-        print(vt_out[i], ofs);
-    ofs << count << endl;
-    ofs.close();
-    closeFile(ifs);
-    return ACC;
-}
+//int main() {
+//    fstream ifs;
+//    fstream ofs;
+//    string str = "start.txt";
+//    if (!openFile(ifs, str, ios::in)) {
+//        cout << "file is not opened" << endl;
+//        return ERR;
+//    }
+//    str = "result.txt";
+//    if (!openFile(ofs, str, ios::out)) {
+//        cout << "file is not opened" << endl;
+//    }
+//
+//    int *pointer;
+//    table s0{}, target{};
+//    pointer = &(s0.table[0][0]);
+//    for (int i = 0; i < 9; i++) {
+//        ifs >> *pointer;
+//        if (*pointer++ == 0) {
+//            s0.row = i / 3;
+//            s0.col = i % 3;
+//        }
+//    }
+//    pointer = &(target.table[0][0]);
+//    for (int i = 0; i < 9; i++) {
+//        ifs >> *pointer;
+//        if (*pointer++ == 0) {
+//            target.row = i / 3;
+//            target.col = i % 3;
+//        }
+//    }
+//
+//    vector<table> vt_out;
+//    int count=puzzle(s0, target, vt_out, false);
+//
+//    if(count==ERR){
+//        ofs << "目标状态不可达" << endl;
+//        return ERR;
+//    }
+//
+//    for(int i=0;i<vt_out.size();i++)
+//        print(vt_out[i], ofs);
+//    ofs << count << endl;
+//    ofs.close();
+//    closeFile(ifs);
+//    return ACC;
+//}
--- statistic.cpp
+++ statistic.cpp
@@ -5,54 +5,54 @@
 #include "puzzle.h"
 #include <algorithm>
 
-//int main() {
-//    fstream ifs;
-//    fstream ofs;
-//    string str = "start.txt";
-//    if (!openFile(ifs, str, ios::in)) {
-//        cout << "file is not opened" << endl;
-//        return ERR;
-//    }
-//    str = "statistic.txt";
-//    if (!openFile(ofs, str, ios::out)) {
-//        cout << "file is not opened" << endl;
-//    }
-//
-//    int *pointerS, *pointerT;
-//    table s0{}, target{};
-//    pointerS = &(s0.table[0][0]);
-//    for (int i = 0; i < 9; i++) {
-//        ifs >> *(pointerS + i);
-//        if (*(pointerS + i) == 0) {
-//            s0.row = i / 3;
-//            s0.col = i % 3;
-//        }
-//    }
-//    pointerT = &(target.table[0][0]);
-//    for (int i = 0; i < 9; i++)
-//        *(pointerT + i) = i;
-//    vector<table> vt_out{};
-//    int count;
-//    int max=0;
-//
-//    for(int i=0;i<44466;i++)
-//        next_permutation(pointerT, pointerT+9);
-//    do {
-//        count = puzzle(s0, target, vt_out, true);
-//        for (int i = 0; i < 9; i++)
-//            ofs << *(pointerS + i) << ' ';
-//        ofs << '\t';
-//        for (int i = 0; i < 9; i++)
-//            ofs << *(pointerT + i) << ' ';
-//        ofs << '\t';
-//        if(max<count)
-//            max=count;
-//        if (count == ERR)
-//            ofs << "目标状态不可达" << endl;
-//        else
-//            ofs << count << endl;
-//
-//    } while (next_permutation(pointerT, pointerT + 9));
-//    cout<<max<<endl;
-//    return ACC;
-//}
+int main() {
+    fstream ifs;
+    fstream ofs;
+    string str = "start.txt";
+    if (!openFile(ifs, str, ios::in)) {
+        cout << "file is not opened" << endl;
+        return ERR;
+    }
+    str = "statistic.txt";
+    if (!openFile(ofs, str, ios::out)) {
+        cout << "file is not opened" << endl;
+    }
+
+    int *pointerS, *pointerT;
+    table s0{}, target{};
+    pointerS = &(s0.table[0][0]);
+    for (int i = 0; i < 9; i++) {
+        ifs >> *(pointerS + i);
+        if (*(pointerS + i) == 0) {
+            s0.row = i / 3;
+            s0.col = i % 3;
+        }
+    }
+    pointerT = &(target.table[0][0]);
+    for (int i = 0; i < 9; i++)
+        *(pointerT + i) = i;
+    vector<table> vt_out{};
+    int count;
+    int max=0;
+
+    for(int i=0;i<44466;i++)
+        next_permutation(pointerT, pointerT+9);
+    do {
+        count = puzzle(s0, target, vt_out, true);
+        for (int i = 0; i < 9; i++)
+            ofs << *(pointerS + i) << ' ';
+        ofs << '\t';
+        for (int i = 0; i < 9; i++)
+            ofs << *(pointerT + i) << ' ';
+        ofs << '\t';
+        if(max<count)
+            max=count;
+        if (count == ERR)
+            ofs << "目标状态不可达" << endl;
+        else
+            ofs << count << endl;
+
+    } while (next_permutation(pointerT, pointerT + 9));
+    cout<<max<<endl;
+    return ACC;
+}
